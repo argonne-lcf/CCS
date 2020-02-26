03e536ae6d30d08471da4b700c5295591f4622d3
Brice Videau <bvideau@anl.gov>
2020-02-26 10:34:25 -0600

    Refactoring.

--- src/cconfigspace.h
+++ src/cconfigspace.h
@@ -1,101 +1,24 @@
-typedef struct cs_configuration_space_s *cs_configuration_space_t;
-typedef struct cs_configuration_s *cs_configuration_t;
-typedef struct cs_hyper_parameter_s *cs_hyper_parameter_t;
-typedef struct cs_condition_s *cs_condition_t;
-typedef struct cs_forbidden_clause_s *cs_forbidden_clause_t;
+#include "ccs/configuation_space.h"
 
-enum cs_error_e {
-  CS_SUCCESS
+typedef struct ccs_configuration_space_s *ccs_configuration_space_t;
+typedef struct ccs_configuration_s       *ccs_configuration_t;
+typedef struct ccs_hyper_parameter_s     *ccs_hyper_parameter_t;
+typedef struct ccs_condition_s           *ccs_condition_t;
+typedef struct ccs_forbidden_clause_s    *ccs_forbidden_clause_t;
+
+enum ccs_error_e {
+  CCS_SUCCESS
 };
 
-typedef enum cs_error_e cs_error_t;
+typedef enum ccs_error_e ccs_error_t;
+
+union ccs_data_u {
+  double      d;
+  int         i;
+  const char *s;
+};
+
+typedef union ccs_data_u ccs_data_t;
 
 
-// Configuration Space Interface
-
-//   Creators/deletion
-extern cs_error_t
-cs_create_configuration_space(cs_configuration_space_t *configuration_space_ret);
-
-extern cs_error_t
-cs_release_configuration_space(cs_configuration_space_t configuration_space);
-
-//   Accessors
-extern cs_error_t
-cs_configuration_space_set_name(cs_configuration_space_t  configuration_space,
-                                const char               *name);
-
-extern cs_error_t
-cs_configuration_space_get_name(cs_configuration_space_t   configuration_space,
-                                char                     **name);
-
-extern cs_error_t
-cs_configuration_space_set_user_data(cs_configuration_space_t  configuration_space,
-                                     void                     *user_data);
-
-extern cs_error_t
-cs_configuration_space_get_user_data(cs_configuration_space_t   configuration_space,
-                                     void                     **user_data);
-
-extern cs_error_t
-cs_configuration_space_set_seed(cs_configuration_space_t configuration_space,
-                                int64_t                  seed);
-
-extern cs_error_t
-cs_configuration_space_get_seed(cs_configuration_space_t  configuration_space,
-                                int64_t                  *seed);
-
-//   List Accessors
-extern cs_error_t
-cs_configuration_space_add_hyper_parameter(cs_configuration_space_t config_space,
-                                           cs_hyper_parameter_t     hyper_parameter);
-
-extern cs_error_t
-cs_configuration_space_add_hyper_parameters(cs_configuration_space_t  config_space,
-                                            size_t                    num_hyper_parameters,
-                                            cs_hyper_parameter_t     *hyper_parameters);
-
-extern cs_error_t
-cs_configuration_space_get_num_hyper_parameters(cs_configuration_space_t  config_space,
-                                                size_t                   *num_hyper_parameters);
-
-extern cs_error_t
-cs_configuration_space_get_hyper_parameter(cs_configuration_space_t config_space,
-                                           size_t                   index);
-
-extern cs_error_t
-cs_configuration_space_get_hyper_parameters(cs_configuration_space_t  config_space,
-                                            size_t                    num_hyper_parameters,
-                                            cs_hyper_parameter_t     *hyper_parameters,
-                                            size_t                   *num_hyper_parameters_ret);
-
-extern cs_error_t
-cs_configuration_space_add_condition(cs_configuration_space_t config_space,
-                                     cs_condition_t           condition);
-
-extern cs_error_t
-cs_configuration_space_add_conditions(cs_configuration_space_t  config_space,
-                                      size_t                    num_conditions,
-                                      cs_condition_t           *conditions);
-
-extern cs_error_t
-cs_configuration_space_get_conditions(cs_configuration_space_t  config_space,
-                                      size_t                    num_conditions,
-                                      cs_condition_t           *conditions,
-                                      size_t                   *num_conditions_ret);
-
-extern cs_error_t
-cs_configuration_space_add_forbidden_clause(cs_configuration_space_t config_space,
-                                            cs_forbidden_clause_t    forbidden_clause);
-
-extern cs_error_t
-cs_configuration_space_add_forbidden_clauses(cs_configuration_space_t  config_space, 
-                                             size_t                    num_forbidden_clauses,
-                                             cs_forbidden_clause_t    *forbidden_clauses);
-
-extern cs_error_t
-cs_configuration_space_get_forbidden_clauses(cs_configuration_space_t  config_space, 
-                                             size_t                    num_forbidden_clauses,
-                                             cs_forbidden_clause_t    *forbidden_clauses
-                                             size_t                   *num_forbidden_clauses_ret);
 
---
+++ src/ccs/configuration_space.h
@@ -0,0 +1,179 @@
+#include "cconfigspace.h"
+
+// Configuration Space Interface
+
+//   Creators/deletion
+extern ccs_error_t
+ccs_create_configuration_space(ccs_configuration_space_t *configuration_space_ret);
+
+extern ccs_error_t
+ccs_release_configuration_space(ccs_configuration_space_t configuration_space);
+
+//   Accessors
+extern ccs_error_t
+ccs_configuration_space_set_name(ccs_configuration_space_t  configuration_space,
+                                 const char                *name);
+
+extern ccs_error_t
+ccs_configuration_space_get_name(ccs_configuration_space_t   configuration_space,
+                                 char                      **name);
+
+extern ccs_error_t
+ccs_configuration_space_set_user_data(ccs_configuration_space_t  configuration_space,
+                                      void                      *user_data);
+
+extern ccs_error_t
+ccs_configuration_space_get_user_data(ccs_configuration_space_t   configuration_space,
+                                      void                      **user_data);
+
+extern ccs_error_t
+ccs_configuration_space_set_seed(ccs_configuration_space_t configuration_space,
+                                 int64_t                   seed);
+
+extern ccs_error_t
+ccs_configuration_space_get_seed(ccs_configuration_space_t  configuration_space,
+                                int64_t                    *seed);
+
+//   List Accessors
+extern ccs_error_t
+ccs_configuration_space_add_hyper_parameter(ccs_configuration_space_t configuration_space,
+                                            ccs_hyper_parameter_t     hyper_parameter);
+
+extern ccs_error_t
+ccs_configuration_space_add_hyper_parameters(ccs_configuration_space_t  configuration_space,
+                                             size_t                     num_hyper_parameters,
+                                             ccs_hyper_parameter_t     *hyper_parameters);
+
+extern ccs_error_t
+ccs_configuration_space_get_num_hyper_parameters(ccs_configuration_space_t  configuration_space,
+                                                 size_t                    *num_hyper_parameters_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_hyper_parameter(ccs_configuration_space_t configuration_space,
+                                            size_t                    index,
+                                            ccs_hyper_parameter_t    *hyper_parameter_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_hyper_parameters(ccs_configuration_space_t  configuration_space,
+                                             size_t                     num_hyper_parameters,
+                                             ccs_hyper_parameter_t     *hyper_parameters,
+                                             size_t                    *num_hyper_parameters_ret);
+
+extern ccs_error_t
+ccs_configuration_space_add_condition(ccs_configuration_space_t configuration_space,
+                                      ccs_condition_t           condition);
+
+extern ccs_error_t
+ccs_configuration_space_add_conditions(ccs_configuration_space_t  configuration_space,
+                                       size_t                     num_conditions,
+                                       ccs_condition_t           *conditions);
+
+extern ccs_error_t
+ccs_configuration_space_get_num_conditions(ccs_configuration_space_t  configuration_space,
+                                           size_t                    *num_conditions_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_condition(ccs_configuration_space_t configuration_space,
+                                      size_t                    index,
+                                      ccs_condition_t          *condition_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_conditions(ccs_configuration_space_t  configuration_space,
+                                       size_t                     num_conditions,
+                                       ccs_condition_t           *conditions,
+                                       size_t                    *num_conditions_ret);
+
+extern ccs_error_t
+ccs_configuration_space_add_forbidden_clause(ccs_configuration_space_t configuration_space,
+                                             ccs_forbidden_clause_t    forbidden_clause);
+
+extern ccs_error_t
+ccs_configuration_space_add_forbidden_clauses(ccs_configuration_space_t  configuration_space,
+                                              size_t                     num_forbidden_clauses,
+                                              ccs_forbidden_clause_t    *forbidden_clauses);
+
+extern ccs_error_t
+ccs_configuration_space_get_num_forbidden_clauses(ccs_configuration_space_t  configuration_space,
+                                                  size_t                    *num_forbidden_clauses_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_forbidden_clause(ccs_configuration_space_t configuration_space,
+                                             size_t                    index,
+                                             ccs_forbidden_clause_t   *forbidden_clause_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_forbidden_clauses(ccs_configuration_space_t  configuration_space,
+                                              size_t                     num_forbidden_clauses,
+                                              ccs_forbidden_clause_t    *forbidden_clauses,
+                                              size_t                    *num_forbidden_clauses_ret);
+
+//   Configuration related functions
+extern ccs_error_t
+ccs_configuration_space_check_configuration(ccs_configuration_space_t configuration_space,
+                                            ccs_configuration_t       configuration);
+
+extern ccs_error_t
+ccs_configuration_space_check_configuration_vector(ccs_configuration_space_t  configuration_space,
+                                                   size_t                     num_datum,
+                                                   ccs_data_t                *vector);
+
+extern ccs_error_t
+ccs_configuration_space_get_default_configuration(ccs_configuration_space_t  configuration_space,
+                                                  ccs_configuration_t       *configuration);
+
+extern ccs_error_t
+ccs_configuration_space_sample_configuration(ccs_configuration_space_t  configuration_space,
+                                             ccs_configuration_t       *configuration);
+
+extern ccs_error_t
+ccs_configuration_space_sample_configurations(ccs_configuration_space_t  configuration_space,
+                                              size_t                     num_configurations,
+                                              ccs_configuration_t       *configurations);
+
+//   Hyperparameter related functions
+extern ccs_error_t
+ccs_configuration_space_get_active_hyperparameters(ccs_configuration_space_t  configuration_space,
+                                                   size_t                     num_hyper_parameters,
+                                                   ccs_hyper_parameter_t     *hyper_parameters,
+                                                   size_t                    *num_hyper_parameters_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_conditional_hyperparameters(ccs_configuration_space_t  configuration_space,
+                                                        size_t                     num_hyper_parameters,
+                                                        ccs_hyper_parameter_t     *hyper_parameters,
+                                                        size_t                    *num_hyper_parameters_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_unconditional_hyperparameters(ccs_configuration_space_t  configuration_space,
+                                                          size_t                     num_hyper_parameters,
+                                                          ccs_hyper_parameter_t     *hyper_parameters,
+                                                          size_t                    *num_hyper_parameters_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_child_conditions_of(ccs_configuration_space_t  configuration_space,
+                                                ccs_hyper_parameter_t      hyper_parameter,
+                                                size_t                     num_conditions,
+                                                ccs_condition_t           *conditions,
+                                                size_t                    *num_conditions_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_children_of(ccs_configuration_space_t  configuration_space,
+                                        ccs_hyper_parameter_t      hyper_parameter,
+                                        size_t                     num_hyper_parameters,
+                                        ccs_hyper_parameter_t     *hyper_parameters,
+                                        size_t                    *num_hyper_parameters_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_parent_conditions_of(ccs_configuration_space_t  configuration_space,
+                                                 ccs_hyper_parameter_t      hyper_parameter,
+                                                 size_t                     num_conditions,
+                                                 ccs_condition_t           *conditions,
+                                                 size_t                    *num_conditions_ret);
+
+extern ccs_error_t
+ccs_configuration_space_get_parents_of(ccs_configuration_space_t  configuration_space,
+                                       ccs_hyper_parameter_t      hyper_parameter,
+                                       size_t                     num_hyper_parameters,
+                                       ccs_hyper_parameter_t     *hyper_parameters,
+                                       size_t                    *num_hyper_parameters_ret);
+
