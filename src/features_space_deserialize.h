c10daf2cdee2b23f4d88aa4d5ea1b1034d42edb9
Brice Videau <bvideau@anl.gov>
2022-02-08 17:46:50 -0600

    Do not add sub-objects to map.

--- src/features_space_deserialize.h
+++ src/features_space_deserialize.h
@@ -2,7 +2,6 @@
 #define _FEATURES_SPACE_DESERIALIZE_H
 #include "context_deserialize.h"
 
-
 static inline ccs_result_t
 _ccs_deserialize_bin_features_space(
 		ccs_features_space_t               *features_space_ret,
@@ -10,9 +9,12 @@ _ccs_deserialize_bin_features_space(
 		size_t                             *buffer_size,
 		const char                        **buffer,
 		_ccs_object_deserialize_options_t  *opts) {
+	_ccs_object_deserialize_options_t new_opts = *opts;
+	new_opts.map_values = CCS_FALSE;
+	new_opts.handle_map = NULL;
 	_ccs_object_internal_t obj;
 	ccs_object_t handle;
-	ccs_result_t res;
+	ccs_result_t res = CCS_SUCCESS;
 	CCS_VALIDATE(_ccs_deserialize_bin_ccs_object_internal(
 		&obj, buffer_size, buffer, &handle));
 	if (CCS_UNLIKELY(obj.type != CCS_FEATURES_SPACE))
@@ -20,7 +22,7 @@ _ccs_deserialize_bin_features_space(
 
 	_ccs_context_data_mock_t data;
 	CCS_VALIDATE_ERR_GOTO(res, _ccs_deserialize_bin_ccs_context_data(
-		&data, version, buffer_size, buffer, opts), end);
+		&data, version, buffer_size, buffer, &new_opts), end);
 	CCS_VALIDATE_ERR_GOTO(res, ccs_create_features_space(
 		data.name, obj.user_data, features_space_ret), end);
 	CCS_VALIDATE_ERR_GOTO(res, ccs_features_space_add_hyperparameters(
